a0cb8ecff35fc7696a158590eea86cfb35981acf
Jeremy Horwitz <jhmail@gmail.com>
2023-06-11 20:23:47 -0700

    keymap horwitz: store list of yellows in an array (instead of using a bit set); only build the array for layers 1, 3; include--commented out--debug code

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/horwitz/keymap.c
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/horwitz/keymap.c
@@ -14,16 +14,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include QMK_KEYBOARD_H
-#include <limits.h>		/* for CHAR_BIT */
+/*
+ * to debug, uncomment "// ADD FOR DEBUGGING" comments and make sure (horwitz/)rules.mk contains "CONSOLE_ENABLE = yes";
+ * the use uprintf to print debug output
+ */
 
-// bit-set logic (from https://c-faq.com/misc/bitsets.html)
-#define BITMASK(b) (1 << ((b) % CHAR_BIT))
-#define BITSLOT(b) ((b) / CHAR_BIT)
-#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
-#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
-#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
-#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+#include QMK_KEYBOARD_H
+//#include "print.h" // ADD FOR DEBUGGING
 
 // clang-format off
 enum layers{
@@ -34,6 +31,16 @@ enum layers{
 };
 #define NUM_LAYERS 4 // TODO? derive from [layers]
 #define NUM_KEYS 84 // TODO? derive from... somewhere
+
+/* // ADD FOR DEBUGGING
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+*/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_ansi_84(
@@ -70,15 +77,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 /*
- * initializes [layer_used_indices] as a bit set representing which indices (0-83) are used in the given [layer]
- * ("used" = not KC_TRNS)
+ * sets the values in [layer_used_indices] to the indices (0-83, in increasing order) that are used in the given [layer]
+ * ("used" = not KC_TRNS) and returns how many values were set. (e.g., if keys at index 0, 5, 83 are the only ones set,
+ * layer_used_indices will start { 0, 5, 83, ... } (followed by 0s) and [initialize_layer_used_indices] will return 3.)
  *
  * why [offset]? rgb_matrix_set_color's first argument appears to be based on an index of keys _not_ including
  * KC_NO--there are 96 (=[MATRIX_ROWS] * [MATRIX_COLS]) keycodes in each keymaps[layer], but (apparently) the non-KC_NO
  * keys are indexed 0-83 (and there are 96-84=12 instances of KC_NO (per layer))... see LAYOUT_ansi_84's definition in
  * obj_keychron_k3_pro_ansi_rgb/src/default_keyboard.h
  */
- void initialize_layer_used_indices(int layer, char* layer_used_indices) {
+ int initialize_layer_used_indices(int layer, char* layer_used_indices) {
+    int lui_i = 0;
     int offset = 0;
     for (int i = 0; i < MATRIX_ROWS * MATRIX_COLS; ++i) {
         int col = i % MATRIX_COLS;
@@ -90,16 +99,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 ++offset;
                 break;
             default:
-                BITSET(layer_used_indices, i - offset);
+                layer_used_indices[lui_i++] = i - offset;
                 break;
         }
     }
+
+    return lui_i;
 }
 
-char layers_used_indices[NUM_LAYERS][BITNSLOTS(NUM_KEYS)];
+char layers_used_indices[NUM_LAYERS][NUM_KEYS];
 // TODO handle this differently(?) (e.g., just initialize all 4 immediately, rather than lazily (and with (presumably)
 //      less-readable code)
-bool layer_used_indices_is_initialized[NUM_LAYERS] = { false };
+int layer_used_indices_size[NUM_LAYERS] = { -1, -1, -1, -1 };
 
 bool rgb_matrix_indicators_user(void) {
     uint8_t layer = biton32(layer_state);
@@ -107,22 +118,19 @@ bool rgb_matrix_indicators_user(void) {
     // when fn is held down, set lights yellow on keys whose behavior changed from base layer; set others to blue
     // inspired by https://www.reddit.com/r/olkb/comments/kpro3p/comment/h3nb56h
 
-    // just initialize layers_used_indices[layer] (at most) once per layer
-    if (!layer_used_indices_is_initialized[layer]) {
-        initialize_layer_used_indices(layer, layers_used_indices[layer]);
-        layer_used_indices_is_initialized[layer] = true;
-    }
     switch (layer) {
         case MAC_BASE:
         case WIN_BASE:
             break;
         case MAC_FN:
         case WIN_FN:
+            // just initialize layers_used_indices[layer] (at most) once per layer
+            if (layer_used_indices_size[layer] == -1) {
+                layer_used_indices_size[layer] = initialize_layer_used_indices(layer, layers_used_indices[layer]);
+            }
 //            rgb_matrix_set_color_all(255, 255, 0); // uncomment to have transparent keys appear solid blue
-            for (int i = 0; i < NUM_KEYS; ++i) {
-                if (BITTEST(layers_used_indices[layer], i)) {
-                    rgb_matrix_set_color(i, 255, 255, 0);
-                }
+            for (int i = 0; i < layer_used_indices_size[layer]; ++i) {
+                rgb_matrix_set_color(layers_used_indices[layer][i], 255, 255, 0);
             }
             break;
     }
